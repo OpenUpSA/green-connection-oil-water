168e2b435f4a2c38392140726c3c3b1bfc24e711
paulmwatson <github.com@paulmwatson.com>
2024-08-21 16:51:10 +0200

    Image path localisation

--- app/[locale]/how-does-it-work/page.tsx
+++ app/[locale]/how-does-it-work/page.tsx
@@ -2,10 +2,13 @@
 
 import { useTranslations } from "next-intl";
 import { Link } from "app/[locale]/navigation";
+import Image from "next/image";
 
-import { useEffect } from "react";
-
-export default function Page() {
+export default function Page({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
   const t = useTranslations("global");
   const tP = useTranslations("how-does-it-work");
   const scenarios = t.raw("scenarios");
@@ -15,57 +18,24 @@ export default function Page() {
       <h1>{tP("title")}</h1>
       <h2>{tP("sub-title")}</h2>
       <p>{tP.rich("intro")}</p>
+      <h3>{tP("your-role.heading")}</h3>
+      <p className="m-9 pb-6 sm:mb-8 sm:mt-7">
+        <Image
+          src={`/images/${locale}/how-does-it-work/your-role.png`}
+          alt="Image of silhouetted portraits"
+          width={950}
+          height={839}
+          sizes="(max-width: 479px) 457.5625px, (max-width: 767px) 100vw, (max-width: 991px) 93vw, 900px"
+          className="display-block max-w-lg"
+        />
+        {tP("your-role.content")}
+      </p>
       <Link
         href={`/scenarios/${scenarios[0].slug}`}
         className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
       >
         {tP("get-started")}
       </Link>
-      <p>
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmod
-      </p>
-      <p>
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmod
-      </p>
-      <p>
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmodlorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-        eiusmod
-      </p>
     </>
   );
 }
